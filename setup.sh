af9d6803ed267ba65d5752ff1448caf4794a9c10
CatEek <zubarev.ia@gmail.com>
2023-08-16 16:49:40 +0200

    updated dataset link

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
+echo -e "\n creating mamba env... \n"
+mamba env create -f environment.yml
+jupyter nbextension enable --py widgetsnbextension
+mamba activate 06_instance_segmentation
 
 echo -e "\n downloading data...\n"
-wget https://www.dropbox.com/s/f0b9uubjf3l1bdy/woodshole.zip
-unzip woodshole.zip
-rm woodshole.zip
-
-echo -e "\n creating conda env... \n"
-conda env create -f environment.yml
-jupyter nbextension enable --py widgetsnbextension
+aws s3 cp "s3://dl-at-mbl-2023-data/woodshole_new.zip" "." --no-sign-request
+unzip woodshole_new.zip
+rm woodshole_new.zip
