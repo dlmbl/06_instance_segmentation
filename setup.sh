2729f5808a7ff9786280d4e4d97eaad87fb54a48
CatEek <zubarev.ia@gmail.com>
2023-08-19 13:47:03 +0200

    path fix, minor errors

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 echo -e "\n creating mamba env... \n"
-mamba env create -f environment.yml
-jupyter nbextension enable --py widgetsnbextension
-mamba activate 06_instance_segmentation
+# mamba env create -f environment.yml
+# jupyter nbextension enable --py widgetsnbextension
+# mamba activate 06_instance_segmentation
 
 echo -e "\n downloading data...\n"
 aws s3 cp "s3://dl-at-mbl-2023-data/woodshole_new.zip" "." --no-sign-request
 unzip woodshole_new.zip
+mkdir woodshole
+mv woodshole_new/* woodshole
 rm woodshole_new.zip
