c73b2d803c7694d6fb4fb0cf17a6c174a6bb475a
William Patton <wllmpttn24@gmail.com>
2024-08-23 15:32:31 -0400

    remove jupytext block at top of solution.py

--- solution.py
+++ solution.py
@@ -1,15 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     cell_metadata_filter: tags,tas,-all
-#     custom_cell_magics: kql
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.11.2
-# ---
-
 # %% [markdown]
 # # Exercise 05: Instance Segmentation :)
 #
